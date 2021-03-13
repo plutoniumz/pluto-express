c49f3cacf89bbebf92c2b5e88af36bfb9c9311e3
Gin <nanolyscl@gmail.com>
2021-03-13 16:23:41 +0700

    Add id to sendMessage response

--- src/middlewares/response/responses/sendMessage.js
+++ src/middlewares/response/responses/sendMessage.js
@@ -1,5 +1,6 @@
 module.exports = function (message) {
     return this.status(200).send({
+        id: session.hasEmployee(this) ? session.getEmployee(this).id : 0,
         message: message,
     })
 }
