9582438bf709be350797d51f67bd0fc1464d81e0
Gin <nanolyscl@gmail.com>
2021-05-09 21:01:32 +0700

    Update 1.2.26: Update cors Hook.

--- src/hooks/cors/hook.js
+++ src/hooks/cors/hook.js
@@ -5,34 +5,44 @@ class CorsHook extends Hook {
         const { allow_domains, allow_key } = this.configs
 
         const getOptions = function (req, callback) {
-            if (
-                req.method === 'OPTIONS' &&
-                req.get('access-control-request-headers') &&
-                req.get('access-control-request-headers').includes(allow_key)
-            ) {
-                callback(null, {
-                    origin: true,
-                    credentials: true,
-                    optionsSuccessStatus: 200,
-                })
-            } else if (
-                req.method !== 'OPTIONS' &&
-                req.get('access-control-request-headers') === 'allow'
-            ) {
-                callback(null, {
-                    origin: true,
-                    credentials: true,
-                    optionsSuccessStatus: 200,
-                })
-            } else {
-                const isDev = allow_domains.indexOf(req.header('Origin')) !== -1
+            // allow_key cors configs
+            if (allow_key !== null && allow_key !== '') {
+                // case method OPTIONS for preflight request
+                if (
+                    req.method === 'OPTIONS' &&
+                    req.get('access-control-request-headers') &&
+                    req
+                        .get('access-control-request-headers')
+                        .includes(allow_key.toLowerCase())
+                ) {
+                    callback(null, {
+                        origin: true,
+                        credentials: true,
+                        optionsSuccessStatus: 200,
+                    })
+                }
 
-                callback(null, {
-                    origin: isDev,
-                    credentials: isDev,
-                    optionsSuccessStatus: isDev ? 200 : 500,
-                })
+                // case another methods
+                if (
+                    req.method !== 'OPTIONS' &&
+                    req.get(allow_key.toLowerCase()) === 'allow'
+                ) {
+                    callback(null, {
+                        origin: true,
+                        credentials: true,
+                        optionsSuccessStatus: 200,
+                    })
+                }
             }
+
+            // default
+            const isDev = allow_domains.indexOf(req.header('Origin')) !== -1
+
+            callback(null, {
+                origin: isDev,
+                credentials: isDev,
+                optionsSuccessStatus: isDev ? 200 : 500,
+            })
         }
 
         const cors = require('cors')(getOptions)
--- src/hooks/cors/index.js
+++ src/hooks/cors/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     configs: {
         allow_domains: ['http://localhost:3000'],
+        allow_key: null,
     },
 }
